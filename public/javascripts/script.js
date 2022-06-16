c2e9a04762ce5394417ec4c903b89072a65905c4
simonCohen36 <94045477+simonCohen36@users.noreply.github.com>
2022-06-16 14:44:46 +0300

    added click listener to each slot

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,14 @@
 const box = document.querySelector('.box')
 const slots = document.querySelectorAll('.slot')
 
-const init = () => {
+const handleClick = () => {
 
+}
+
+const init = () => {
+    slots.forEach(slot => {
+        slot.addEventListener('click', handleClick)
+    })
 }
 
 init()
