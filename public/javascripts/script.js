74fc5c6b5055eb0c3a61115a54d7665b619a4f02
simonCohen36 <94045477+simonCohen36@users.noreply.github.com>
2022-06-16 15:13:27 +0300

    added function to shuffle the values in the game

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,26 @@
 const box = document.querySelector('.box')
 const slots = document.querySelectorAll('.slot')
+const PAIRS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 const handleClick = () => {
 
 }
 
+const shuffleElements = () => {
+    let elements = PAIRS.concat(PAIRS)
+    let currentIndex = elements.length, temporaryValue, randomIndex
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+        temporaryValue = elements[currentIndex]
+        elements[currentIndex] = elements[randomIndex]
+        elements[randomIndex] = temporaryValue
+    }
+    return elements
+}
+
 const init = () => {
+    let mixedValues = shuffleElements()
     slots.forEach(slot => {
         slot.addEventListener('click', handleClick)
     })
